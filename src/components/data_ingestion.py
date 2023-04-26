023db8a057dad06e922e16be5b6efbcfb5383077
anurag0009 <anurag.anurag.singh864@gmail.com>
2023-04-26 22:23:07 +0530

    file updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -47,10 +47,13 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-
-if __name__ == "__main__":
-    obj = DataIngestion()
+if __name__=="__main__":
+    obj=DataIngestion()
     #obj.initiate_data_ingestion()
-    train_data,test_data = obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
     data_transformation.initiate_data_transformation(train_data,test_data)
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-from src.logger import logging
 import dill
-import pickle
 
 
 
@@ -13,10 +11,11 @@ import pickle
 def save_object(file_path ,obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
