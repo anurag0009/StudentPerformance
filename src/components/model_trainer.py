cc3e56ca4473a969910a844d7d6acb43d7da5cf2
anurag0009 <anurag.anurag.singh864@gmail.com>
2023-04-27 15:13:34 +0530

    file updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,16 +37,53 @@ class ModelTrainer:
 
             models={
                 "Random_forest" : RandomForestRegressor(),
-                "Desision_tree" : DecisionTreeRegressor(),
+                "Decision_tree" : DecisionTreeRegressor(),
                 "Gradient_Boosting" : GradientBoostingRegressor(),
                 "Linear_Regression" : LinearRegression(),
                 "K-Neighbors_Classifiers" : KNeighborsRegressor(),
                 "XGBRegressior": XGBRegressor(),
-                "CatBoosting_" : CatBoostRegressor(),
-                "AdaBoodt" : AdaBoostRegressor()
+                "CatBoosting" : CatBoostRegressor(),
+                "AdaBoost" : AdaBoostRegressor()
+            }
+            param={
+                "Decision_tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random_forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient_Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear_Regression":{},
+                "XGBRegressior":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
-            model_report:dict = evaluate_model(X_train=X_train,y_train= y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict = evaluate_model(X_train=X_train,y_train= y_train,X_test=X_test,y_test=y_test,models=models,param=param)
 
 
             ## to get best model score from dict
